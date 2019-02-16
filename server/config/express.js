2c25ef902124e80c1df9eb3f006d5324c5fe2e63
indation_elina_guo <elina.guo@indation.com>
2019-02-16 16:30:27 +0800

    Modify the direct

--- server/config/express.js
+++ server/config/express.js
@@ -80,7 +80,7 @@ module.exports = function () {
 
   // Setting the app router and static folder
   app.use('/', express.static(path.resolve('../manager_web/www')));
-  // app.use('/manager', express.static(path.resolve('../manager_web/www')));
+  app.use('/client', express.static(path.resolve('../../yuyue-vue-app/yuyue-client/dist')));
   app.use(function (req, res, next) {
 
     // Environment dependent middleware
