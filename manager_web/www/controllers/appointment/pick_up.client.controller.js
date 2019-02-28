46b30cb09eeb4ba13e6836c381a13f9e412d33e2
indation_elina_guo <elina.guo@indation.com>
2019-02-28 23:48:22 +0800

    Clear code

--- manager_web/www/controllers/appointment/pick_up.client.controller.js
+++ manager_web/www/controllers/appointment/pick_up.client.controller.js
@@ -96,7 +96,9 @@ angular.module('YYWeb').controller('AppointmentPickUpController',
         console.log(photoname1);
 
         var photobase = plugin().Base64Photo;
-        $scope.IDImage = 'data:image/jpeg;base64,' + photobase;
+        if(photobase){
+          $scope.IDImage = 'data:image/jpeg;base64,' + photobase;
+        }
         //console.log(photobase);
         // myElement10.src="data:image/jpeg;base64,"+photobase;
       };
