e52f4ec4841628d93b19e4a24d7ca673f1831312
indation_elina_guo <elina.guo@indation.com>
2019-03-03 09:42:46 +0800

    Clear code

--- manager_web/www/controllers/pay/list.client.controller.js
+++ manager_web/www/controllers/pay/list.client.controller.js
@@ -39,50 +39,15 @@ angular.module('YYWeb').controller('PayListController',
               alert('page changed!');
             });
           }
-        },
-        groupList: []
-      };
-
-
-      $scope.goBack = function () {
-        $window.history.back();
-      };
-
-
-      $scope.goState = function (state) {
-        if (!state) {
-          return;
-        }
-        $state.go(state);
-      };
-      $scope.goToView = function (state) {
-        if (!state) {
-          return;
-        }
-
-        switch (state) {
-          case 'user_manager':
-            return $state.go('user_manager');
-          case 'restaurant':
-            $state.go('goods_manager', {goods_type: 'dish'});
-            return;
-          case 'supermarket':
-            if (user.role === 'admin' || user.role === 'supermarket_manager') {
-              $state.go('supermarket_order');
-            }
-            return;
-          default:
-            return;
         }
       };
+
 
       function init() {
 
         $scope.$emit(GlobalEvent.onShowLoading, true);
         loadAppointments(function(){
-          $scope.pageConfig.pagination.totalCount = 3;
           $scope.$emit(GlobalEvent.onShowLoading, false);
-          alert('init');
         });
       }
 
