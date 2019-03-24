4ca1f38d3ef4208090dbfe9c0e111c21d296b3a8
indation_elina_guo <elina.guo@indation.com>
2019-03-24 15:47:57 +0800

    Fix bug

--- manager_web/www/controllers/appointment/pick_up.client.controller.js
+++ manager_web/www/controllers/appointment/pick_up.client.controller.js
@@ -25,7 +25,7 @@ angular.module('YYWeb').controller('AppointmentPickUpController',
           timeRange: new Date(item.start_time).Format('yyyy/MM/dd hh:mm') + '~' + new Date(item.end_time).Format('hh:mm'),
           paid: item.paid,
           status: item.status,
-          mobile: item.member.mobile_phone,
+          mobile: item.member.mobile_phone || '未绑定',
           statusString: AppointmentService.translateAppointmentStatus(item.status),
           picked: item.picked
         };
