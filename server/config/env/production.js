a6b70dfb0c5c310291564ae97a47c55b31318b10
Elina <elina.guo@indation.com>
2019-08-26 22:16:41 +0800

    Modify the product config

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -8,13 +8,13 @@ module.exports = {
   app: {
     title: 'YUYUE-SYSTEM - Production Environment'
   },
-  serverAddress:'http://datonghao.com/',
+  serverAddress:'http://localhost/',
   port: process.env.PORT || 80,
   wechat_ext: {//医院公众号信息
     app_id: 'wx84f82babe25f6b05',
     app_secret: 'df2d1036887d6a2ecfc28d20eca0fe64'
   },
   sql: {
-    host: 'mh-rjgb.com'
+    host: '172.28.30.246'//医院内部门诊报告数据库host
   }
 };
